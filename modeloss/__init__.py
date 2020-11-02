61def68248e3949a1a047f09168242084bbf2072
Ouail <28654657+okitouni@users.noreply.github.com>
2020-11-02 00:39:33 -0500

    Refactoring and setup

---
+++ modeloss/__init__.py
@@ -0,0 +1,10 @@
+"""
+MoDe-Loss.
+
+Moment Decorrelation (MoDe) is a tool that can enforce decorrelation between some nuisance parameter (or protected attribute in ML fairness lingo) and the response of some model with gradient-based optimization (a neural network for example.) It can force trained models to have the same response across different values of the protected attribute but it can also go beyond simple decorrelation. For example, MoDe can constrain the response function to be linear or quadratic in the protected attribute. This can increase performance tremendously if the independence constraint is not necessary but instead one only cares about the "smoothness" of the dependence of the response on the protected attribute (which is a weaker constraint.)
+
+For more details please see: https://arxiv.org/abs/2010.09745
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Ouail Kitouni'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='modeloss',
+    version='0.1.0',    
+    description='Moment Decorrelation for constrained NN outputs with respect to protected attributes. (Decorrelated (flat), linear, quadratic, etc.)',
+    url='https://github.com/okitouni/MoDe',
+    author='Ouail Kitouni',
+    author_email='kitouni@gmail.com',
+    license='BSD 2-clause',
+    packages=['modeloss'],
+    install_requires= ['numpy','scipy'],
+    extras_require = {
+    'torch':  ['torch>=1.6.0'],
+    'tf': ['tensorflow>=2.2.0']
+    },
+    python_requires='>=3.6',
+    )
