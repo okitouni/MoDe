4ae67352d5d9fc31cb1223703f8491f59bf7f709
Ouail <28654657+okitouni@users.noreply.github.com>
2020-11-02 01:22:56 -0500

    Refactoring and setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='modeloss',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Moment Decorrelation for constrained NN outputs with respect to protected attributes. (Decorrelated (flat), linear, quadratic, etc.)',
     url='https://github.com/okitouni/MoDe',
     author='Ouail Kitouni',
     author_email='kitouni@gmail.com',
     license='BSD 2-clause',
     packages=['modeloss'],
-    install_requires= ['numpy','scipy','torch>=1.6.0','tensorflow>=2.2.0'],
+    install_requires= ['numpy','scipy','torch','tensorflow'],
     python_requires='>=3.6',
     )
